064e81da99bb4ae64ecc6e8d812491b4f679e1b0
asheesh@asheesh.org <asheesh@asheesh.org>
2009-03-30 03:32:35 +0000

    Add a finite state machine-style filter to remove duplicate color codes

--- playerpiano.py
+++ playerpiano.py
@@ -8,6 +8,62 @@ homepage: http://playerpiano.googlecode.com/
 Original idea & minor tty frobage from Ian Bicking.  Thanks Ian!
 """
 
+import string
+
+class UselessColorFilter(object):
+    def __init__(self):
+        self.state = 'START'
+        self.growing_color = ''
+        self.last_color = None
+    def handle_char(self, char):
+        if self.state == 'START':
+            if char in string.printable:
+                # Always emit the color we have been growing; usually empty string
+                return char
+            else:
+                assert char == '\x1b'
+                assert self.growing_color == ''
+                self.growing_color += char
+                self.state = 'JUST_GOT_X1B'
+        elif self.state == 'JUST_GOT_X1B':
+            assert self.growing_color == '\x1b'
+            assert char == '['
+            self.growing_color += char
+            self.state = 'GROWING_COLOR'
+        elif self.state == 'GROWING_COLOR':
+            if char == 'm':
+                # Then the color is finished!
+                # Was the color we just grew the last_color?
+                # Tack on the space for good measure.
+                self.growing_color += char
+
+                final_color = self.growing_color
+                should_print_this = (self.growing_color != self.last_color)
+
+                # do some cleanup
+                self.last_color = final_color
+                self.growing_color = ''
+
+                # We go back to the START
+                self.state = 'START'
+
+                if should_print_this:
+                    return final_color
+            else:
+                self.growing_color += char
+
+def process_string(s):
+    generated = []
+    ucf = UselessColorFilter()
+    for char in s:
+        ret = ucf.handle_char(char)
+        if ret is None:
+            continue
+        else:
+            generated.append(ret)
+    return generated
+
+
 import doctest
 import termios
 import tty
@@ -124,8 +180,13 @@ def main():
                 # highlight
                 source = highlight(source, lexer, formatter)
 
+                # "fix it up"
+                source = process_string(source)
+
                 # strip trailing newline - added back below
-                source = source.rstrip()
+                assert source[-2] != '\r'
+                if source[-1] == '\n':
+                    source = source[:-1]
                 
                 # write out source code one keypress at a time
                 write('>>> ')
