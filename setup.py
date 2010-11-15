8a91584b3931b1d568af281f9ea3f672cf33bc7b
Peter Fein <pfein@pobox.com>
2010-11-15 13:56:40 -0800

    package

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@
 
 from setuptools import setup, find_packages
 
+# this horrible mess brought to you by the crap that is Python distutils. Just use CPAN.
+version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
+VERSION = open(version_file).read().strip().split('.')
+release = '.'.join(VERSION)
+
 setup(name='PlayerPiano',
-      version='0.1',
-      description='PlayerPiano amazes your friends by running Python doctests in a fake interactive shell.',
+      author = 'Peter Fein',
+      author_email = 'pfein@pobox.com',
+      version=version,
+      description='Amazes your friends by running Python doctests in a fake interactive shell.',
       author='Peter Fein',
       author_email='pfein@pobox.com',
       url='http://playerpiano.googlecode.com',
@@ -16,4 +23,14 @@ setup(name='PlayerPiano',
       packages=find_packages(),
       include_package_data = True,
       install_requires=['pygments'],
-     )
+      license="BSD",
+      long_description=open("README").read(),
+      classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Topic :: Education :: Computer Aided Instruction (CAI)",
+        "Topic :: System :: Shells",
+        ],
+)
