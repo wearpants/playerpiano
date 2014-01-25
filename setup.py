90183a1ea97a2314d6088214fb75ddb4555317b6
Jason R. Coombs <jaraco@jaraco.com>
2014-01-25 10:50:13 -0500

    Normalize indentation

--- setup.py
+++ setup.py
@@ -8,28 +8,29 @@ version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
 VERSION = open(version_file).read().strip().split('.')
 release = '.'.join(VERSION)
 
-setup(name='PlayerPiano',
-      version=release,
-      description='Amazes your friends by running Python doctests in a fake interactive shell.',
-      author='Peter Fein',
-      author_email='pete@wearpants.org',
-      url='https://github.com/wearpants/playerpiano',
-      entry_points = {
-          'console_scripts': [
-              'playerpiano=playerpiano.piano:main',
-              'recorderpiano=playerpiano.recorder:main',
-          ]},
-      packages=find_packages(),
-      include_package_data = True,
-      install_requires=['pygments'],
-      license="BSD",
-      long_description=open("README.rst").read(),
-      classifiers = [
+setup(
+    name='PlayerPiano',
+    version=release,
+    description='Amazes your friends by running Python doctests in a fake interactive shell.',
+    author='Peter Fein',
+    author_email='pete@wearpants.org',
+    url='https://github.com/wearpants/playerpiano',
+    entry_points = {
+        'console_scripts': [
+            'playerpiano=playerpiano.piano:main',
+            'recorderpiano=playerpiano.recorder:main',
+        ]},
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires=['pygments'],
+    license="BSD",
+    long_description=open("README.rst").read(),
+    classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Operating System :: POSIX",
         "Topic :: Education :: Computer Aided Instruction (CAI)",
         "Topic :: System :: Shells",
-        ],
+    ],
 )
